8a41e17dd41b8ef8e38fe406e426fbf693a825d7
sidneyFernandez <sidneyfernandez19@yahoo.es>
2021-10-13 21:19:00 -0500

    se agrega Update para categoria y productos

--- categoria.php
+++ categoria.php
@@ -6,7 +6,7 @@
 
 /*-----------------Inicia BLOQUE de Codigo para capturar datos de Productos----------------------------------------*/
 
-/* capturamos los valores obtenidos en el formulario Categoria y lo guardamos en dos variables */
+/* capturamos los valores obtenidos en el formulario Productos y lo guardamos en dos variables */
 $txtcodig=(isset($_POST['txtcodig']))?$_POST['txtcodig']:"";
 $txtnombre=(isset($_POST['txtnombre']))?$_POST['txtnombre']:"";
 $txtdescripcion=(isset($_POST['txtdescripcion']))?$_POST['txtdescripcion']:"";
@@ -89,10 +89,71 @@ switch($ejecutar) {
           
                      break;  
 
-        
+            case "btnModificarP":
+
+                
+                /*Bloque de codigo para modificar los productos */
+
+
+                $cade=$pdo->prepare("UPDATE productos SET nombre=:nombre
+                ,descripcion=:descripcion,
+                categoria=:categoria,
+                disponible=:disponible,
+                precio=:precio
+                 WHERE codigo=:codigo");
+                    /*Pasamos los valores que tenemos almacenados en las variables temporales*/
+                    
+                    $cade->bindParam(':codigo',$txtcodig);
+                     $cade->bindParam(':nombre',$txtnombre);
+                     $cade->bindParam(':descripcion',$txtdescripcion);
+                    $cade->bindParam(':categoria',$txtdisponible);
+                     $cade->bindParam(':disponible',$txtcategoria);
+                     $cade->bindParam(':precio',$txtprecio);
+
+
+                     $cade->execute();
+
+                    
+
+                   /*Instruccion para controlar conflicto por imagenes con nombre iguales */
+                    $Fecha = new DateTime(); /*Define una variable de tipo tiempo */
+
+                    $nombreArchivo=($txtfoto!="")?$Fecha->getTimestamp()."_".$_FILES["txtfoto"]["name"]:"imagen.jpg";
+
+                    $tmpImagen= $_FILES["txtfoto"]["tmp_name"];
+
+                    
+
+                    /*Condicion pae evaluar si la variable es diferente, en caso afirmativo se asigna con un nuevo nombre */
+                    if($tmpImagen!=""){
+                        move_uploaded_file ($tmpImagen,"imagenes/".$nombreArchivo);
+
+
+                        $cade=$pdo->prepare("UPDATE productos SET imagen=:imagen WHERE codigo=:codigo");
+                     
+                        $cade->bindParam(':codigo',$txtcodig);
+                        $cade->bindParam(':imagen',$nombreArchivo);
+
+                        $cade->execute();
+                    }
 
 
 
+
+
+
+                    
+
+                     header('Location: categoria.php');
+
+                
+                
+                break;
+
+                case "btnEliminarP":
+
+
+                   
      }   
          
 
@@ -131,15 +192,41 @@ switch($accion) {
 
         break;
      case "btnModificar":
-        echo "Presionaste boton Modificar";
-        echo $txtCodigo;
-        echo $txtNombre;
+        
+            /*Sentencia SQL para modificar datos */
+
+             $cadena=$pdo->prepare("UPDATE categoria SET nombre=:nombre WHERE codigo=:codigo");
+
+             /*Pasamos los valores que tenemos almacenados en las variables temporales*/
+ 
+             $cadena->bindParam(':codigo',$txtCodigo);
+             $cadena->bindParam(':nombre',$txtNombre);
+ 
+             $cadena->execute();
+
+            /*Para evitar que se reenvie el formulario usamos la siguiente instruccion */
+
+            header('Location: categoria.php');
+
+
+
+
+
          break;
 
     case "btnEliminar":
-        echo "Presionaste boton Eliminar";
-        echo $txtCodigo;
-        echo $txtNombre;
+        
+
+
+
+       
+
+
+
+
+
+
+
 
             break;
 
@@ -247,10 +334,10 @@ $listaProduct=$cnsltProduct->fetchALL(PDO::FETCH_ASSOC);
         <div class="form-row">
 
             <label for="">Codigo</label>
-            <input class="form-control" type="text" name="txtCodigo" placeholder="" id="txtCodigo" require="" autocomplete="off">
+            <input class="form-control" type="text" name="txtCodigo" value= "<?php echo $txtCodigo; ?>"  placeholder="" id="txtCodigo" require="" autocomplete="off">
             <br>
             <label for="">Nombre:</label>
-            <input class="form-control" type="text" name="txtNombre" placeholder="" id="txtCodigo" require="" autocomplete="off">
+            <input class="form-control" type="text" name="txtNombre" value= "<?php echo $txtNombre; ?>" placeholder="" id="txtCodigo" require="" autocomplete="off">
             <br>
 
         </div>
@@ -290,11 +377,11 @@ $listaProduct=$cnsltProduct->fetchALL(PDO::FETCH_ASSOC);
    
 
     
-    <div class="modal fade" id="exampleModal2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="exampleModal2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel2" aria-hidden="true">
          <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Productos</h5>
+                <h5 class="modal-title" id="exampleModalLabel2">Productos</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -306,21 +393,21 @@ $listaProduct=$cnsltProduct->fetchALL(PDO::FETCH_ASSOC);
        
 
         <label for="">Codigo</label>
-            <input class="form-control" type="text" name="txtcodig" placeholder="" id="txtcodig" require="" autocomplete="off">
+            <input class="form-control" type="text" name="txtcodig"  value= "<?php echo $txtcodig; ?>" placeholder="" id="txtcodig" require="" autocomplete="off">
             <br>
             <label for="">Nombre:</label>
-            <input class="form-control" type="text" name="txtnombre" placeholder="" id="txtnombre" require="" autocomplete="off">
+            <input class="form-control" type="text" name="txtnombre"  value= "<?php echo $txtnombre; ?>" placeholder="" id="txtnombre" require="" autocomplete="off">
             <br>
 
             <label for="">Descripcion:</label>
                 <br>
-            <input class="form-control" type="text" name="txtdescripcion" placeholder="" id="txtdescripcion" require="" autocomplete="off">
+            <input class="form-control" type="text" name="txtdescripcion" value= "<?php echo $txtdescripcion; ?>"  placeholder="" id="txtdescripcion" require="" autocomplete="off">
 
             <br>
             
 
             
-                 <select class="form-control" name="txtdisponible" id="txtdisponible"  aria-label="Default select example" required="" >
+                 <select class="form-control" name="txtdisponible" id="txtdisponible"  value= "<?php echo $txtdisponible; ?>" aria-label="Default select example" required="" >
                      <br>
                              <option selected>Disponible</option>
                                             <option value="Si">Si</option>
@@ -331,7 +418,7 @@ $listaProduct=$cnsltProduct->fetchALL(PDO::FETCH_ASSOC);
              <br>
              <br>
 
-                <select class="form-control" name="txtcategoria" id="txtcategoria"  aria-label="Default select example" required="">
+                <select class="form-control" name="txtcategoria" id="txtcategoria" value= "<?php echo $txtcategoria; ?>" aria-label="Default select example" required="">
                     <br>
                         <option selected="">Categoria:</option>
                     <?php
@@ -348,12 +435,12 @@ $listaProduct=$cnsltProduct->fetchALL(PDO::FETCH_ASSOC);
 
             <label for="">Precio:</label>
             
-            <input class="form-control" type="text" name="txtprecio" placeholder="" id="txtprecio" require="" autocomplete="off">
+            <input class="form-control" type="text" name="txtprecio" placeholder="" id="txtprecio"  value= "<?php echo $txtprecio; ?>" require="" autocomplete="off">
             <br>
 
 
             <label for="">Foto:</label>
-            <input class="form-control" type="file" accept="imge/*" name="txtfoto" placeholder="" id="txtfoto" require="" autocomplete="off">
+            <input class="form-control" type="file" accept="imge/*" name="txtfoto" placeholder="" id="txtfoto" value= "<?php echo $txtfoto; ?>" require="" autocomplete="off">
             <br>
 
 
@@ -408,6 +495,7 @@ $listaProduct=$cnsltProduct->fetchALL(PDO::FETCH_ASSOC);
                         <tr>
                             <th scope="col">Codigo Producto</th>
                             <th scope="col">Nombre Producto</th>
+                            <th scope="col">Acciones</th>
                         </tr>
                     </thead>
 
@@ -415,7 +503,20 @@ $listaProduct=$cnsltProduct->fetchALL(PDO::FETCH_ASSOC);
                         <tr>
                             <td> <?php echo $listaC['codigo']; ?> </td>
                             <td> <?php echo $listaC['nombre']; ?> </td>
+                            <td> 
+                                
+                            <form action ="" method= "post">
 
+                            <input type="hidden" name="txtCodigo" value= "<?php echo $listaC['codigo']; ?>">
+                            <input type="hidden" name="txtNombre" value="<?php echo $listaC['nombre']; ?>">
+
+                        
+                           <input   type="submit" value="Seleccionar" name="accion">
+                          
+                            </form>
+                            </td>
+                            
+                            
 
                         </tr>
 
@@ -429,6 +530,7 @@ $listaProduct=$cnsltProduct->fetchALL(PDO::FETCH_ASSOC);
         </div>
 
 
+      
 <!-- tabla que me permite mostrar el contenido de la tabla Categoria Fin de bloque -->
         
 
@@ -454,7 +556,7 @@ $listaProduct=$cnsltProduct->fetchALL(PDO::FETCH_ASSOC);
                         <th scope="col">Disponible</th>
                         <th scope="col">Precio</th>
                         <th scope="col">Imagen</th>
-
+                        <th scope="col">Acciones</th>
                     </tr>
                 </thead>
 
@@ -468,6 +570,27 @@ $listaProduct=$cnsltProduct->fetchALL(PDO::FETCH_ASSOC);
                         <td> <?php echo $listaP['precio']; ?> </td>
                         <td><img class="img-thumbnail" width="100px"src="Imagenes/<?php echo $listaP['imagen']; ?>"/>  </td>
 
+
+                        <!--Bloque de codigo que permite seleccionar registro de la tabla productos y enviarlos al formulario-->
+                        <td> 
+                                
+                            <form action ="" method= "post">
+
+                            <input type="hidden" name="txtcodig" value= "<?php echo $listaP['codigo']; ?>">
+                            <input type="hidden" name="txtnombre" value="<?php echo $listaP['nombre']; ?>">
+                            <input type="hidden" name="txtdescripcion" value="<?php echo $listaP['descripcion']; ?>">
+                            <input type="hidden" name="txtcategoria" value="<?php echo $listaP['categoria']; ?>">
+                            <input type="hidden" name="txtdisponible" value="<?php echo $listaP['disponible']; ?>">
+                            <input type="hidden" name="txtprecio" value="<?php echo $listaP['precio']; ?>">
+                            <input type="hidden" name="txtfoto" value="<?php echo $listaP['imagen']; ?>">
+
+                        
+                                <input   type="submit" value="Seleccionar" name="ejecutar">
+
+                                
+                            </form>
+                            </td>
+                        <!--Finaliza Bloque de codigo seleccinar registro productos para enviarlo a formulario de podructos-->
 
                     </tr>
 
